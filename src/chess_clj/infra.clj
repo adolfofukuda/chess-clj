0bb7b2a21c5697116b879c75ff824edba5bea80e
Adolfo Noriyuki Fukuda <adolfo.fukuda@gmail.com>
2013-09-23 00:04:21 -0300

    some improvements with errors

--- src/chess_clj/infra.clj
+++ src/chess_clj/infra.clj
@@ -1,24 +1,26 @@
-
 (ns chess-clj.infra
   (:require  [clojure.contrib.math :refer :all]
              [clojure.contrib.string :refer :all]
              [clojure.contrib.combinatorics :refer :all]))
-
-
-(defrecord Chessboard [a1 a2 a3 a4 a5 a6])
+(defrecord Chessboard
+  [a8 b8 c8 d8 e8 f8 g8 h8
+   a7 b7 c7 d7 e7 f7 g7 h7
+   a6 b6 c6 d6 e6 f6 g6 h6
+   a5 b5 c5 d5 e5 f5 g5 h5
+   a4 b4 c4 d4 e4 f4 g4 h4
+   a3 b3 c3 d3 e3 f3 g3 h3
+   a2 b2 c2 d2 e2 f2 g2 h2
+   a1 b1 c1 d1 e1 f1 g1 h1])
 
 (defn init-chessboard []
-  (hash-map :a8 "r" :b8 "n" :c8 "b" :d8 "q" :e8 "k" :f8 "b" :g8 "n" :h8 "r"
-            :a7 "p" :b7 "p" :c7 "p" :d7 "p" :e7 "p" :f7 "p" :g7 "p" :h7 "p"
-            :a6 "" :b6 "" :c6 "" :d6 "" :e6 "" :f6 "" :g6 "" :h6 ""
-            :a5 "" :b5 "" :c5 "" :d5 "" :e5 "" :f5 "" :g5 "" :h5 ""
-            :a4 "" :b4 "" :c4 "" :d4 "" :e4 "" :f4 "" :g4 "" :h4 ""
-            :a3 "" :b3 "" :c3 "" :d3 "" :e3 "" :f3 "" :g3 "" :h3 ""
-            :a2 "P" :b2 "P" :c2 "P" :d2 "P" :e2 "P" :f2 "P" :g2 "P" :h2 "P"
-            :a1 "R" :b1 "N" :c1 "B" :d1 "Q" :e1 "K" :f1 "B" :g1 "N" :h1 "R"))
-
-
-;(def movements (ref []))
+   (->Chessboard "r" "n" "b" "q" "k" "b" "n" "r"
+                 "p" "p" "p" "p" "p" "p" "p" "p"
+                 "" "" "" "" "" "" "" ""
+                 "" "" "" "" "" "" "" ""
+                 "" "" "" "" "" "" "" ""
+                 "" "" "" "" "" "" "" ""
+                 "P" "P" "P" "P" "P" "P" "P" "P"
+                 "R" "N" "B" "Q" "K" "B" "N" "R"))
 
 (defn coordinate? [coordinate]
   (not (nil? (re-matches #"[a-h][1-8]$" coordinate))))
@@ -34,7 +36,7 @@
   (read-string (subs coordinate 1 2)))
 
 (defn lazy-contains? [col piece]
- (some #(= piece %) col))1
+ (some #(= piece %) col))
 
 (defn inc-col [col offset]
   (let [carac (+ offset (apply int (seq (lower-case col))))]
